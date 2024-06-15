f52b2f49351008549561b71031b79f8298ce0e73
buildten <buildten0@gmail.com>
2024-06-15 15:36:45 +0900

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box sx={{ width: '100%'}}>
+        <Typography variant="h1" gutterBottom fontFamily={"AppleSDGothicNeoBlack"}>
+          완전히 새로워지다.
+        </Typography>
+        <Typography variant="h3" gutterBottom fontFamily={"AppleSDGothicNeoBlack"}>
+          새롭다. 완전하다. 놀랍다.
+        </Typography>
+        <Typography variant="h6" gutterBottom fontFamily={"AppleSDGothicNeoBlack"}>
+          iPhone 26은 지금까지 보지 못했던 신선한 기억을 당신에게 선물합니다.
+          <br></br>
+          또한 지구를 위해 Apple의 허울뿐인 환경 보호를 완벽히 실천하죠.
+          <br></br>
+          이는 지금껏 있었던 iPhone 시리즈 중 가장 새로운 iPhone입니다.
+        </Typography>
+      </Box>
     </>
   )
 }
