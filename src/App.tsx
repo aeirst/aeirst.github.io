c67556c26db8b7dae7fdfae2588d8a309a5f4c06
buildten <buildten0@gmail.com>
2024-06-15 16:38:03 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,21 @@ function App() {
     <>
       <Box sx={{ width: '100%'}}>
         <Typography variant="h1" gutterBottom fontFamily={"AppleSDGothicNeoBlack"}>
-          완전히 새로워지다.
+          iPhone 26.
         </Typography>
         <Typography variant="h3" gutterBottom fontFamily={"AppleSDGothicNeoBlack"}>
           새롭다. 완전하다. 놀랍다.
         </Typography>
         <Typography variant="h6" gutterBottom fontFamily={"AppleSDGothicNeoBlack"}>
-          iPhone 26은 지금까지 보지 못했던 신선한 기억을 당신에게 선물합니다.
-          <br></br>
-          또한 지구를 위해 Apple의 허울뿐인 환경 보호를 완벽히 실천하죠.
-          <br></br>
-          이는 지금껏 있었던 iPhone 시리즈 중 가장 새로운 iPhone입니다.
+          iPhone 26은 지금까지 보지 못했던 신선한 iPhone에 대한 기억을 선물합니다.
+          <br/>
+          또한 지구를 위한 Apple의 2030 계획을 적용하여 환경 보호를 완벽히 실천하죠.
+          <br/>
+          iPhone 26은 지금껏 있었던 iPhone 시리즈 중 가장 새로운 iPhone입니다.
         </Typography>
+      </Box>
+      <Box sx={{ width: '100%' }}>
+
       </Box>
     </>
   )
