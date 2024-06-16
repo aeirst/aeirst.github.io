63398437954e0e1b3615b5d54b55f2edd55f9d15
Aqouthe_ <161127101+be-noi@users.noreply.github.com>
2024-06-16 17:49:06 +0900

    ddd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import './App.css'
+import { Gray } from './styled/styled';
+import { Button, Link, styled } from '@mui/material';
+
+const DarkButton = styled(Button)({
+  color: 'white',
+  boxShadow: 'none',
+  textTransform: 'none',
+  fontSize: 16,
+  padding: '6px 12px',
+  border: '1px solid',
+  lineHeight: 1.5,
+  backgroundColor: '#2b2b2b',
+  borderColor: '#000000',
+  fontFamily: 'AppleSDGothicNeoRegular',
+
+  '&:hover': {
+    backgroundColor: '#383838',
+    borderColor: '#1e1e1e',
+    boxShadow: '0 0 0 0.22rem rgba(174, 174, 174, 0.671)',
+  },
+});
+
 
 function App() {
   return (
     <>
-      <Box sx={{ width: '100%'}}>
-        <Typography variant="h1" gutterBottom fontFamily={"AppleSDGothicNeoBlack"}>
-          iPhone 26.
+      <Box sx={{ width: '100%'}} >
+        <Typography variant="h1" gutterBottom fontFamily={"AppleSDGothicNeoBlack"} fontSize={"70px"}>
+          <span className='typing'>aeirst.</span>
         </Typography>
-        <Typography variant="h3" gutterBottom fontFamily={"AppleSDGothicNeoBlack"}>
-          새롭다. 완전하다. 놀랍다.
+        <Typography variant="h3" gutterBottom fontFamily={"AppleSDGothicNeoExtraRegular"}>
+          A normal developer.
         </Typography>
-        <Typography variant="h6" gutterBottom fontFamily={"AppleSDGothicNeoBlack"}>
-          iPhone 26은 지금까지 보지 못했던 신선한 iPhone에 대한 기억을 선물합니다.
+        <Typography variant="h6" gutterBottom fontFamily={"AppleSDGothicNeoRegular"} marginBottom={"20px"}>
+          Aeirst <Gray>is a normal developer from south korea.</Gray>
           <br/>
-          또한 지구를 위한 Apple의 2030 계획을 적용하여 환경 보호를 완벽히 실천하죠.
-          <br/>
-          iPhone 26은 지금껏 있었던 iPhone 시리즈 중 가장 새로운 iPhone입니다.
+          I made <Link href="https://github.com/aeirst/lim" underline='none'>Lim</Link>, <Link href="https://github.com/aeirst/crispy" underline='none'>Crispy</Link>, <Link href="https://github.com/aeirst/simple-ui" underline='none'>Simple/ui</Link>, <Link href="https://github.com/poml-project/poml" underline='none'>Poml</Link>, <Link href="https://github.com/aeirst/depresser" underline='none'>Depresser</Link>.
         </Typography>
-      </Box>
-      <Box sx={{ width: '100%' }}>
+        <DarkButton href="https://github.com/aeirst">Github</DarkButton>&nbsp;&nbsp;&nbsp;
+        <DarkButton href="https://docs.poml.kro.kr">Docs</DarkButton>&nbsp;&nbsp;&nbsp;
+        <DarkButton href="https://discord.poml.kro.kr">Discord</DarkButton>&nbsp;&nbsp;&nbsp;
+        <DarkButton href="https://blog.poml.kro.kr">Blog</DarkButton><br/><br/>
       </Box>
     </>
   )
---
+++ src/styled/styled.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Gray = styled.span`
+    color: #6e6e73;
+`
